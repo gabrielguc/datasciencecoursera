283fe3522b33185fc968eccfaea701148b44545e
Gabriel Gutierrez <gabrielgutierrezcorral@hotmail.com>
2016-02-28 19:06:00 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -14,17 +14,17 @@
 
 # Load all the data required:
 # - Column names
-featureNames <- read.table("./dataset/features.txt")[,2]
+featureNames <- read.table("./UCI HAR Dataset/features.txt")[,2]
 # - Activity labels
-activityLabels <- read.table("./dataset/activity_labels.txt")[,2]
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 # - Train data
-subjectTrain <- read.table("./dataset/train/subject_train.txt", header = FALSE)
-activityTrain <- read.table("./dataset/train/y_train.txt", header = FALSE)
-featuresTrain <- read.table("./dataset/train/X_train.txt", header = FALSE)
+subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+activityTrain <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+featuresTrain <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
 # - Test data
-subjectTest <- read.table("./dataset/test/subject_test.txt", header = FALSE)
-activityTest <- read.table("./dataset/test/y_test.txt", header = FALSE)
-featuresTest <- read.table("./dataset/test/X_test.txt", header = FALSE)
+subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+activityTest <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+featuresTest <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
 # Merge Train and Test data:
 subject <- rbind(subjectTrain, subjectTest)
 activity <- rbind(activityTrain, activityTest)
